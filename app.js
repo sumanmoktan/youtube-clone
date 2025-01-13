16eb0cd0849e520a08cb15de75eb011e2a59443f
sumanmoktan <sumantamang662@gmail.com>
2025-01-13 13:29:04 +0545

    adding model code respectively

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 import express from "express";
+import authRoute from "./routes/auth.js";
+import userRoute from "./routes/user.js";
+import VideoRoute from "./routes/video.js";
+import commentRoute from "./routes/comment.js";
+
 const app = express();
+
+//routes
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/video", VideoRoute);
+app.use("/api/v1/comment", commentRoute);
 
 export default app;
---
+++ controllers/userController.js
@@ -0,0 +1,7 @@
+export const test = (req, res) => {
+    const test = 
+  res.status(200).json({
+    status: "success",
+    message: "test successfull",
+  });
+};
